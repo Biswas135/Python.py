deb32f356a38640ba152b3bd57f880ae4368521b
Biswas135 <Biswasbasyal135@gmail.com>
2025-12-07 12:44:43 +0545

    Add files via upload

---
+++ 2). String and conditional statment/16) str int float.py
@@ -0,0 +1,13 @@
+# String Input 
+
+# take name input (string)
+name = input("Enter your name : ")
+
+# take age input (converted to int)
+age = int(input("Enter your age : "))
+
+# take price input (converted to float)
+price = float(input("Enter your price : "))
+
+# print all the values
+print("My name is :", name, "My age is :", age, "Price is :", price)
