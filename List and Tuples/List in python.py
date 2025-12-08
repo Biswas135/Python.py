d78c1e8d96ddcc458432c6995d3016bbe2ac543b
Biswas135 <Biswasbasyal135@gmail.com>
2025-12-08 11:20:46 +0545

    Add example of list usage in Python

---
+++ List and Tuples/List in python.py
@@ -0,0 +1,6 @@
+marks = [94.4, 72.4, 89.55, 45.23, 64.2]   # creating a list of marks
+print(marks)                               # print the entire list
+print(type(marks))                         # print the data type (list)
+print(marks[0])                            # print the first element
+print(marks[1])                            # print the second element
+print(len(marks))                          # print the number of items in the list
