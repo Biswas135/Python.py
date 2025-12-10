caecb4bdfaa0437134b959c1c62a10225325cc0c
Biswas135 <Biswasbasyal135@gmail.com>
2025-12-10 13:35:19 +0545

    Add print_hello function to print a message

---
+++ function & recursion/function0.py
@@ -0,0 +1,4 @@
+def print_hello():          # function definition (no parameters)
+    print("Hello World")    # function body that prints a message
+
+print_hello()               # calling the function
