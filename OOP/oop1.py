0e3f3d310d5db1e5388808595a9716d95c3c1adf
Biswas135 <Biswasbasyal135@gmail.com>
2025-12-11 11:42:37 +0545

    Add Student class with example objects

---
+++ OOP/oop1.py
@@ -0,0 +1,11 @@
+# Creating a class named Student
+class Student:
+    name = "Biswas"   # Class attribute (same for all objects)
+
+# Creating first object of Student class
+s1 = Student()
+print(s1.name)        # prints "Biswas"
+
+# Creating second object of Student class
+s2 = Student()
+print(s2.name)        # prints "Biswas"
